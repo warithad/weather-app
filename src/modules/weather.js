be49d31d471b93dcda0b9b5247b141ccf20a9caf
warith <warith.ad02@gmail.com>
2022-11-28 00:16:08 +0100

    Implement sending of data request

---
+++ src/modules/weather.js
@@ -0,0 +1,26 @@
+import { API_KEY as key } from './apikey';
+
+export default class Weather {
+    static async getData(city) {
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${key}`;
+        try {
+            const response = await fetch(url, { mode: 'cors' });
+            if (!response.ok) {
+                throw new Error(`${city} not found`);
+            }
+
+            const data = await response.json();
+            const temperature = `Temperature: ${data.main.temp}&#8451`;
+            const feelsLike = `Feels Like: ${data.main.feels_like}&#8451`;
+            const humidity = `Humidity: ${data.main.humidity}%`;
+            const windSpeed = `Wind: ${data.wind.speed} km/h`;
+
+            return {
+                temperature, feelsLike, humidity, windSpeed, city,
+            };
+        } catch (error) {
+            alert(error);
+            return null;
+        }
+    }
+}
